7e2fedab4784627cd914a5b4cd6c2f992a3458ea
Ross Cooperman <ross@bigapple.local>
2010-06-30 15:30:58 -0400

    pared down CassandraMapper::Support:Dependencies module to the bare minimum -- in so doing was able to eliminate a bunch of core extensions that are no longer necessary

--- lib/cassandra_mapper/support/inflector.rb
+++
@@ -1,7 +0,0 @@
-# in case active_support/inflector is required without the rest of active_support
-require 'cassandra_mapper/support/inflector/inflections'
-require 'cassandra_mapper/support/inflector/transliterate'
-require 'cassandra_mapper/support/inflector/methods'
-
-require 'cassandra_mapper/support/inflections'
-require 'cassandra_mapper/core_ext/string/inflections'
